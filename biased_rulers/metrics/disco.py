fa86f61638f267c52cd5a7d833fa516ef6e7553a
Pieter Delobelle <pieter.delobelle@gmail.com>
2022-07-19 10:05:46 +0200

    Update to DisCo to consider single nouns

--- biased_rulers/metrics/disco.py
+++ biased_rulers/metrics/disco.py
@@ -2,7 +2,7 @@ from biased_rulers.data.nouns import load_data
 from transformers import AutoTokenizer, BertTokenizer, BertModel, pipeline
 import torch
 from collections import Counter
-from scipy.stats import chi2_contingency 
+from scipy.stats import chi2_contingency, chisquare
 
 def disco_test(tokenizer: BertTokenizer, model: BertModel):
     """
@@ -19,9 +19,9 @@ def disco_test(tokenizer: BertTokenizer, model: BertModel):
     # TODO: figure out if the double nouns matter
     # TODO: find out if extra data matters
     for template in templates:
-        x_tokens, y_tokens = [], []
         template = template.replace("BLANK", tokenizer.mask_token)
         for noun in nouns.iterrows():
+            x_tokens, y_tokens = [], []
             for x in pipe(template.replace("PERSON", noun[1][0]), top_k=3):
                 x_tokens.append(x['token_str'])
             for x in pipe(template.replace("PERSON", noun[1][1]), top_k=3):
@@ -29,24 +29,26 @@ def disco_test(tokenizer: BertTokenizer, model: BertModel):
         #print( Counter(x_tokens) )
         #print( Counter(y_tokens) )
         
-        x_counter, y_counter = Counter({x: 0 for x in set(y_tokens)}), Counter({x: 0 for x in set(x_tokens)})
-        x_counter.update(x_tokens)
-        y_counter.update(y_tokens)
-        x_counts = [x[1] for x in sorted(x_counter.items(), key=lambda pair: pair[0], reverse=False)]
-        y_counts = [x[1] for x in sorted(y_counter.items(), key=lambda pair: pair[0], reverse=False)]
+            x_counter, y_counter = Counter({x: 0 for x in set(y_tokens)}), Counter({x: 0 for x in set(x_tokens)})
+            x_counter.update(x_tokens)
+            y_counter.update(y_tokens)
+            x_counts = [x[1] for x in sorted(x_counter.items(), key=lambda pair: pair[0], reverse=False)]
+            y_counts = [x[1] for x in sorted(y_counter.items(), key=lambda pair: pair[0], reverse=False)]
 
-        # We test with a X^2 test.
-        # The null hypothesis is that gender is independent of each predicted token.
-        print(x_counts, y_counts)
-        chi, p, dof, _ = chi2_contingency([x_counts, y_counts]) 
-    
-        print(dof)
+            # We test with a X^2 test.
+            # The null hypothesis is that gender is independent of each predicted token.
+            print(x_counter, y_counter)
+            print(x_counts, y_counts)
+            chi, p = chisquare(x_counts, y_counts) 
         
-        significance_level = 0.05
-        print("p value: " + str(p)) 
-        if p <= significance_level: 
-            print('Reject H0') 
-        else: 
-            print('ACCEPT H0') 
+            print(dof)
+        
+            significance_level = 0.05
+            print("p value: " + str(p)) 
+            if p <= significance_level: 
+                print(f"{noun[1][0]}: Reject H0") 
+            else: 
+                pass
+                #print(f"{noun[1][0]}: accept H0") 
             
     return 
--- biased_rulers/tests/metrics/test_disco.py
+++ biased_rulers/tests/metrics/test_disco.py
@@ -12,9 +12,6 @@ def test_disco_bert_base_names():
     model = AutoModel.from_pretrained(model_type)
     print(f"Loaded {model_type}")
 
-    attribute_template = "This is the _."
-    target_template = "This is the _."
-
     score = disco.disco_test(tokenizer, model)
 
     # Score for the bert-base names test (taken from https://arxiv.org/pdf/2010.06032.pdf)
