48179ed28a72054db9742aa161f949e4a7d4c801
Pieter Delobelle <pieter.delobelle@gmail.com>
2022-07-26 15:44:45 +0200

    Added extra gendered word list

--- biased_rulers/data/nouns.py
+++ biased_rulers/data/nouns.py
@@ -7,3 +7,9 @@ def load_data():
 
     df = pd.read_csv("biased_rulers/data/generalized_swaps.txt", sep="\t", header=None)
     return df
+
+def load_extra_data():
+    "Load the extra list of gendered nouns from Zhao et al. (2018)."
+
+    df = pd.read_csv("biased_rulers/data/extra_gendered_words.txt", sep="\t", header=None)
+    return df
