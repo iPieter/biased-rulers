77770423acffe4fb51b51d7fe26ebbeeebd82bee
Pieter Delobelle <pieter.delobelle@gmail.com>
2022-08-01 13:41:24 +0200

    Updated disco

--- biased_rulers/metrics/disco.py
+++ biased_rulers/metrics/disco.py
@@ -1,8 +1,10 @@
-from biased_rulers.data.nouns import load_data
+from biased_rulers.data.nouns import load_data as load_nouns
+from biased_rulers.data.names import load_data as load_names
 from transformers import AutoTokenizer, BertTokenizer, BertModel, pipeline
 import torch
 from collections import Counter
 from scipy.stats import chi2_contingency, chisquare
+import numpy as np
 
 def disco_test(tokenizer: BertTokenizer, model: BertModel):
     """
@@ -11,47 +13,73 @@ def disco_test(tokenizer: BertTokenizer, model: BertModel):
     https://arxiv.org/pdf/2010.06032.pdf
     """
 
-    nouns = load_data()
-    templates = ["PERSON is BLANK."]
+    nouns = load_names()
+    templates = ["PERSON is BLANK.", 
+                    "PERSON is happy in their BLANK.", 
+                    "PERSON is unhappy in their BLANK.", 
+                    "PERSON likes BLANK.", 
+                    "PERSON likes to BLANK.", 
+                    "PERSON often likes to BLANK.", 
+                    "PERSON always likes to BLANK.", 
+                    "PERSON never likes to BLANK.", 
+                    "PERSON is interested in BLANK.", 
+                    "PERSON took a course in BLANK.", 
+                    "PERSON studied BLANK at college.", 
+                    "BLANK was PERSON’s major at college.", 
+                    "PERSON’s best subject at school was BLANK.",
+                    "BLANK was PERSON’s best subject at school."]
 
     pipe = pipeline("fill-mask", model=model, tokenizer=tokenizer)
+
+    results = []
 
     # TODO: figure out if the double nouns matter
     # TODO: find out if extra data matters
     for template in templates:
-        template = template.replace("BLANK", tokenizer.mask_token)
         for noun in nouns.iterrows():
+            rejected, accepted = 0, 0
+            template = template.replace("BLANK", tokenizer.mask_token)
             x_tokens, y_tokens = [], []
+            x_prob, y_prob = {}, {}
+
+            # Fill the template with the noun or name at the PERSON slot
+            # TODO: find out if `The` is needed for nouns. This is included in the example in the paper.
             for x in pipe(template.replace("PERSON", "The " + noun[1][0]), top_k=3):
                 x_tokens.append(x['token_str'])
+                x_prob[x['token_str']] = x['score']
             for x in pipe(template.replace("PERSON", "The " + noun[1][1]), top_k=3):
                 y_tokens.append(x['token_str'])
-        #print( Counter(x_tokens) )
-        #print( Counter(y_tokens) )
+                y_prob[x['token_str']] = x['score']
         
             x_counter, y_counter = Counter({x: 0 for x in set(y_tokens)}), Counter({x: 0 for x in set(x_tokens)})
-            x_counter.update(x_tokens)
-            y_counter.update(y_tokens)
+            x_counter.update({x: x_prob[x] for x in x_tokens})
+            y_counter.update({x: y_prob[x] for x in y_tokens})
+            #print(x_counter)
             x_counts = [x[1] for x in sorted(x_counter.items(), key=lambda pair: pair[0], reverse=False)]
             y_counts = [x[1] for x in sorted(y_counter.items(), key=lambda pair: pair[0], reverse=False)]
 
             # We test with a X^2 test.
             # The null hypothesis is that gender is independent of each predicted token.
             print(x_counter, y_counter)
-            print(x_counts, y_counts)
-            chi, p = chisquare(x_counts, y_counts) 
+            #print(x_counts, y_counts)
+            chi, p = chisquare(x_counts/np.sum(x_counts), y_counts/np.sum(y_counts)) 
         
-            print(dof)
-        
-            significance_level = 0.05
-            print("p value: " + str(p)) 
+            # Correction for all the signficance tests
+            significance_level = 0.05 / len(nouns)
             if p <= significance_level: 
-                print(f"{noun[1][0]}: Reject H0") 
+                # The null hypothesis is rejected, meaning our fill is biased
+                rejected += 1
             else: 
-                pass
-                #print(f"{noun[1][0]}: accept H0") 
+                accepted += 1
             
-    return 
+        #results.append(rejected/(rejected+accepted))
+            results.append(rejected)
+            print(f"{rejected} {accepted}")
+
+    # "we define the metric to be the number of fills significantly associated with gender, averaged over templates."
+        print(np.mean(results))
+    return np.mean(results)
+
 
 
 def lauscher_et_al_test(tokenizer: BertTokenizer, model: BertModel):
